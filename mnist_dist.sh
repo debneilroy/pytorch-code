3d72f6e4cfb95061abbcaae27ceaaed3d50ad8cf
debneilroy <42752482+debneilroy@users.noreply.github.com>
2018-08-29 01:45:10 -0400

    Update mnist_dist.sh

--- mnist_dist.sh
+++ mnist_dist.sh
@@ -22,32 +22,4 @@ export PYTHONPATH=/sdcc/u/droy/.conda/envs/pytorch_mpi/lib/python3.5/site-packag
 export PATH=/hpcgpfs01/software/openmpi/3.1.1-gnu/bin/:$PATH
 export LD_LIBRARY_PATH=/hpcgpfs01/software/openmpi/3.1.1-gnu/lib/:$LD_LIBRARY_PATH
 
-mpirun -np 2 python mnist_dist_1.py --world-size 2 --dist-backend 'mpi' > mnist_dist_1.txt
-
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- INSERT --                                                                                                                                                                1,1           All
-
--- INSERT --                                                                                                                                                                25,1          All
-
+mpirun -np 2 python mnist_dist.py --world-size 2 --dist-backend 'mpi' > mnist_dist.txt
